2859ca6d253a7c58f1b22629820e0ccb9ff116e3
perlovka288 <jeffkostlim@gmail.com>
2026-05-31 17:07:48 +0300

    deploy portfolio

--- config/db.php
+++ config/db.php
@@ -3,17 +3,19 @@ $host    = 'sql113.infinityfree.com';
 $db      = 'if0_42054781_portfolio_db';
 $user    = 'if0_42054781';
 $pass    = 'zK0B2jMfGj1MX';
-$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => true,
+        ]
+    );
+} catch (PDOException $e) {
+    die('DB Error: ' . $e->getMessage());
 }
 ?>
